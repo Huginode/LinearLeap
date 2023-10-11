0a1ed064fe64528e0d3b7ca0bb9dcce766454135
Huginode <Anonymcoder@protonmail.com>
2023-10-11 23:13:27 +0200

    Definied most of the functionsl, still need to fix a bug

--- code.py
+++ code.py
@@ -4,7 +4,6 @@ from sklearn.datasets import make_regression
 
 #Random regression Graph using sklearn
 x, y = make_regression(n_samples= 100, n_features=1, noise=10)
-plt.scatter(x, y)
 
 # Of course reshaping y dimensions
 y = y.reshape(y.shape[0], 1)
@@ -18,3 +17,14 @@ theta = np.random.randn(2, 1)
 #Def the mode
 def model(X, theta):
     return X.dot(theta)
+
+#test
+plt.scatter(x, y)
+plt.plot(x, model(X, theta), c='r')
+
+# Cost funciton
+def cost_function(X, y, theta):
+    m = len(y)
+    return 1/(2*m) * np,sum((model(X, theta) - y)**2)
+
+cost_function(X, y, theta)
